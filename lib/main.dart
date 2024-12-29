c66619c2a214ebc8ad6107496ba8aff1044a5411
gurumdevv <gurumdevv@gmail.com>
2024-12-29 10:39:30 +0900

    build: Firebase 의존성 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // firebase 앱 시작
   runApp(const MyApp());
 }
 
